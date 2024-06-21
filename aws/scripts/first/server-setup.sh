cfb7a7b6ec151744f0bbd32b385dc635fae875bd
mt-nix <mate.torma@nixs.com>
2024-06-21 15:21:23 +0200

    Update server-setup.sh

--- aws/scripts/first/server-setup.sh
+++ aws/scripts/first/server-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt install nginx
+apt install nginx -y
 rm /etc/nginx/sites-available/default
 curl -L -o /etc/nginx/sites-available/default "https://drive.google.com/u/0/uc?id=1OjKQs-tgNkklHJ7sFDN8g1h5Aa2GgN7Y&export=download"
 rm -rf /var/www/html
