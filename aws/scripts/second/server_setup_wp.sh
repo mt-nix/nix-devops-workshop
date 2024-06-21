0b697795b7adff6bd0095e247ae6616cab0d35ad
mt-nix <mate.torma@nixs.com>
2024-06-21 16:32:22 +0200

    Update server_setup_wp.sh

--- aws/scripts/second/server_setup_wp.sh
+++ aws/scripts/second/server_setup_wp.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 rm /etc/nginx/sites-available/default
 curl -L -o /etc/nginx/sites-available/default "https://raw.githubusercontent.com/mt-nix/nix-devops-workshop/main/aws/files/second/default"
-apt install python-software-properties
-add-apt-repository ppa:ondrej/php
-apt install php8.3 php8.3-fpm
-apt install php-mysql
+apt install python-software-properties -y
+add-apt-repository ppa:ondrej/php -y
+apt install php8.3 php8.3-fpm -y
+apt install php-mysql -y
 mkdir /root/Downloads
 wget -P /root/Downloads https://wordpress.org/wordpress-6.5.3.tar.gz
 cd /root/Downloads
