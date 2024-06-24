11a67d7db7e6b5f0877aca7ed7903e27f9dcc117
mt-nix <mate.torma@nixs.com>
2024-06-24 15:03:00 +0200

    Update server-setup.sh

--- aws/scripts/first/server-setup.sh
+++ aws/scripts/first/server-setup.sh
@@ -5,4 +5,5 @@ curl -L -o /etc/nginx/sites-available/default "https://raw.githubusercontent.com
 rm -rf /var/www/html
 mkdir /var/www/html
 curl -L -o /var/www/html/index.html "https://raw.githubusercontent.com/mt-nix/nix-devops-workshop/main/aws/files/first/index.html"
+curl -L -o /var/www/html/surprise.gif "https://media.tenor.com/rI_0O_9AJ5sAAAAj/nyan-cat-poptart-cat.gif"
 systemctl restart nginx 
