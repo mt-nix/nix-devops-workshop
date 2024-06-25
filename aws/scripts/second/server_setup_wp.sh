a13bd0eead6a17173427ab75f0fe96f55a50f951
mt-nix <mate.torma@nixs.com>
2024-06-25 18:09:36 +0200

    Update server_setup_wp.sh

--- aws/scripts/second/server_setup_wp.sh
+++ aws/scripts/second/server_setup_wp.sh
@@ -10,11 +10,35 @@ apt install python-software-properties -y
 add-apt-repository -y ppa:ondrej/php
 apt install php8.3 php8.3-fpm -y
 apt install php-mysql -y
-mkdir -p /root/Downloads
-wget -P /root/Downloads https://wordpress.org/wordpress-6.5.3.tar.gz
-cd /root/Downloads
-tar -xf wordpress-6.5.3.tar.gz
-cp /root/Downloads/wordpress/* /var/www/html -r
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+
+read -p "Please enter your domain: " domain
+
+if [[ $domain == https://* ]]; then
+  domain="$domain"
+elif [[ $domain == http://* ]]; then
+  domain="https://${domain:7}"
+else
+  domain="https://$domain"
+fi
+
+read -p "Please enter the DB endpoint: " dbendpoint
+read -p "Please enter your DB admin username: " dbusername
+read -sp "Please enter your DB admin password: " dbpassword
+read -p "Please enter your Wordpress admin username: " wpusername
+read -sp "Please enter your Wordpress admin password: " wppassword
+echo
+
+cd /var/www/html
+
+wp core download
+wp core config --dbname="$dbendpoint" --dbuser="$dbusername" --dbpass="$dbpassword"
+wp core install --url="$domain" --title="Test Wordpress Page" --admin_user="$wpusername" --admin_password="$wppassword" --admin_email="email@example.com"
+
 chown www-data:www-data /var/www/html -R
 systemctl restart nginx
 systemctl restart php8.3-fpm
