4ebbdc3e8918a1e49841729ea4eac259f9985c29
mt-nix <mate.torma@nixs.com>
2024-06-26 10:42:53 +0200

    Update server_setup_wp.sh

--- aws/scripts/second/server_setup_wp.sh
+++ aws/scripts/second/server_setup_wp.sh
@@ -1,4 +1,38 @@
 #!/bin/bash
+first_server_setup() {
+    echo "Setting up the first web server..."
+
+    read -p "Please enter your domain: " domain
+
+    if [[ $domain == https://* ]]; then
+    domain="$domain"
+    elif [[ $domain == http://* ]]; then
+    domain="https://${domain:7}"
+    else
+    domain="https://$domain"
+    fi
+
+    read -p "Please enter the DB endpoint: " dbendpoint
+    read -p "Please enter your DB admin username: " dbusername
+    read -sp "Please enter your DB admin password: " dbpassword
+    read -p "Please enter your Wordpress admin username: " wpusername
+    read -sp "Please enter your Wordpress admin password: " wppassword
+    echo
+
+    wp core config --dbname="$dbendpoint" --dbuser="$dbusername" --dbpass="$dbpassword"
+    wp core install --url="$domain" --title="Test Wordpress Page" --admin_user="$wpusername" --admin_password="$wppassword" --admin_email="admin@example.com"
+}
+
+second_server_setup() {
+    echo "Setting up the second web server..."
+
+    read -p "Please enter the DB endpoint: " dbendpoint
+    read -p "Please enter your DB admin username: " dbusername
+    read -sp "Please enter your DB admin password: " dbpassword
+
+    wp core config --dbname="$dbendpoint" --dbuser="$dbusername" --dbpass="$dbpassword"
+}
+
 mkdir /cert
 cd /cert
 openssl genrsa -out server.key 2048
@@ -12,32 +46,26 @@ apt install php8.3 php8.3-fpm -y
 apt install php-mysql -y
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
-
-read -p "Please enter your domain: " domain
-
-if [[ $domain == https://* ]]; then
-  domain="$domain"
-elif [[ $domain == http://* ]]; then
-  domain="https://${domain:7}"
-else
-  domain="https://$domain"
-fi
-
-read -p "Please enter the DB endpoint: " dbendpoint
-read -p "Please enter your DB admin username: " dbusername
-read -sp "Please enter your DB admin password: " dbpassword
-read -p "Please enter your Wordpress admin username: " wpusername
-read -sp "Please enter your Wordpress admin password: " wppassword
-echo
-
 cd /var/www/html
-
 wp core download
-wp core config --dbname="$dbendpoint" --dbuser="$dbusername" --dbpass="$dbpassword"
-wp core install --url="$domain" --title="Test Wordpress Page" --admin_user="$wpusername" --admin_password="$wppassword" --admin_email="email@example.com"
+
+while true; do
+    read -p "Is it the first server you run this script on? Please answer with YES, or NO: " input
+
+    input=$(echo "$input" | tr '[:lower:]' '[:upper:]')
+
+    if [ "$input" == "YES" ]; then
+        first_server_setup
+        break
+    elif [ "$input" == "NO" ]; then
+        second_server_setup
+        break
+    else
+        echo "Please enter either YES or NO."
+    fi
+done
 
 chown www-data:www-data /var/www/html -R
 systemctl restart nginx
