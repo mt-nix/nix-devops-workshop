0195cb4875d80d0b3e0bec503384e1c070300309
mt-nix <mate.torma@nixs.com>
2024-06-26 16:06:24 +0200

    Update setup_second_server.sh

--- aws/scripts/second/setup_second_server.sh
+++ aws/scripts/second/setup_second_server.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-    read -p "Please enter the DB endpoint: " dbendpoint
-    read -p "Please enter your DB admin username: " dbusername
-    read -sp "Please enter your DB admin password: " dbpassword
-    echo
+read -p "Please enter the DB endpoint: " dbendpoint
+read -p "Please enter your DB admin username: " dbusername
+read -sp "Please enter your DB admin password: " dbpassword
+echo
 
-    cd /var/www/html
-    wp config create --dbhost="$dbendpoint" --dbname="wordpress" --dbuser="$dbusername" --dbpass="$dbpassword"
+cd /var/www/html
+
+wp config create --dbhost="$dbendpoint" --dbname="wordpress" --dbuser="$dbusername" --dbpass="$dbpassword"
