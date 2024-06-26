c717b15539f0b8516f0d919b4de95ccc3d5273df
mt-nix <mate.torma@nixs.com>
2024-06-26 16:05:00 +0200

    Update server_setup_wp.sh

--- aws/scripts/second/server_setup_wp.sh
+++ aws/scripts/second/server_setup_wp.sh
@@ -1,39 +1,12 @@
 #!/bin/bash
 first_server_setup() {
     echo "Setting up the first web server..."
-
-    read -p "Please enter your domain: " domain
-
-    if [[ $domain == https://* ]]; then
-    domain="$domain"
-    elif [[ $domain == http://* ]]; then
-    domain="https://${domain:7}"
-    else
-    domain="https://$domain"
-    fi
-
-    read -p "Please enter the DB endpoint: " dbendpoint
-    read -p "Please enter your DB admin username: " dbusername
-    read -sp "Please enter your DB admin password: " dbpassword
-    echo
-    read -p "Please enter your Wordpress admin username: " wpusername
-    read -sp "Please enter your Wordpress admin password: " wppassword
-    echo "DEBUG: BEFORE USER SWITCH"
-    
-    sudo -u ubuntu -i -- bash -c 'cd /var/www/html && echo "DEBUG: AFTER CD" && pwd && \
-    wp config create --dbname="$dbendpoint" --dbuser="$dbusername" --dbpass="$dbpassword" && echo "DEBUG: AFTER WP CORE CONFIG" && \
-    wp core install --url="$domain" --title="Test Wordpress Page" --admin_user="$wpusername" --admin_password="$wppassword" --admin_email="admin@example.com" && echo "DEBUG: AFTER WP CORE INSTALL"'
+    sudo -u ubuntu -i -- bash -c './setup_first_server.sh'
 }
 
 second_server_setup() {
     echo "Setting up the second web server..."
-
-    read -p "Please enter the DB endpoint: " dbendpoint
-    read -p "Please enter your DB admin username: " dbusername
-    read -sp "Please enter your DB admin password: " dbpassword
-    echo
-
-    sudo -u ubuntu -i -- bash -c 'cd /var/www/html && wp config create --dbname="$dbendpoint" --dbuser="$dbusername" --dbpass="$dbpassword"'
+    sudo su ubuntu -i -- bash -c './setup_second_server.sh'
 }
 
 mkdir /cert
@@ -50,6 +23,8 @@ add-apt-repository -y ppa:ondrej/php
 apt install php8.3 php8.3-fpm -y
 apt install php-mysql -y
 
+apt install mysql-client-core-8.0 -y
+
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
@@ -62,9 +37,11 @@ while true; do
     input=$(echo "$input" | tr '[:lower:]' '[:upper:]')
 
     if [ "$input" == "YES" ]; then
+        curl -L -o /home/ubuntu/setup_first_server.sh "https://raw.githubusercontent.com/mt-nix/nix-devops-workshop/main/aws/scripts/second/setup_first_server.sh"
         first_server_setup
         break
     elif [ "$input" == "NO" ]; then
+        curl -L -o /home/ubuntu/setup_second_server.sh "https://raw.githubusercontent.com/mt-nix/nix-devops-workshop/main/aws/scripts/second/setup_second_server.sh"
         second_server_setup
         break
     else
