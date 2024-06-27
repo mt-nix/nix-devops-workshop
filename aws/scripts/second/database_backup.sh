53c89f8e793e54e3560a4495249a282013c62056
mt-nix <mate.torma@nixs.com>
2024-06-27 14:55:26 +0200

    Update database_backup.sh

--- aws/scripts/second/database_backup.sh
+++ aws/scripts/second/database_backup.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-mysqldump -h remote_host -u admin_username -p wordpress > wordpress_backup.sql
+mkdir -p /backup
+
+current_date_time=$(date +"%d_%m_%y_%H_%M")
+filename="/backup/mysql_backup_${current_date_time}.sql"
+
+read -p "S3 bucket name: " s3_bucket
+read -p "RDS database endpoint: " endpoint
+read -p "MySQL admin username: " adminuser
+read -sp "MySQL admin password: " adminpw
+
+mysqldump -h "$endpoint" -u "$adminuser" -p"$adminpw" wordpress > $filename
+aws s3 cp $file_path s3://$s3_bucket/
